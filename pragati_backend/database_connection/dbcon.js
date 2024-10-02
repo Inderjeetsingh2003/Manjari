48fe8189b01f663e89868bf04a8ac6924fd121d3
inderjeetsingh2003 <phenomenalinderjeet2003@gmail.com>
2024-10-02 12:29:14 +0530

    pragati_frontend deleted

--- pragati_backend/database_connection/dbcon.js
+++ pragati_backend/database_connection/dbcon.js
@@ -1,8 +1,2 @@
-const mongoose=require("mongoose")
-mongoose.connect(process.env.DATABASEURL).then(()=>{
-    console.log("database successfully connected ")
-}).catch((error)=>
-{
-console.log(error.message)
-console.log("database not connected")
-})
+
+
