9fe973088b0e723bf0d2dd17f764c89b4ffa2221
Nibha83 <126313367+Nibha83@users.noreply.github.com>
2024-10-09 15:52:21 +0530

    Added hamburger menu
    Sliding bar remaining

--- manjari_frontend/components/Related_Communication/LanguageModule.jsx
+++ manjari_frontend/components/Related_Communication/LanguageModule.jsx
@@ -1,16 +1,26 @@
-import React, { useEffect ,useState,useRef} from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, FlatList,Image } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
-import LanguageCards from './LanguageCards';
-import arrow_left from '@/assets/images/arrow_back_24dp_F5C7C7.png'
- 
-import speakWord from "./SpeakingFunction"
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
+import LanguageCards from "./LanguageCards";
+import arrow_left from "@/assets/images/arrow_back_24dp_F5C7C7.png";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import speakWord from "./SpeakingFunction";
 //import VerticalBar from '../Navigation_Bar/VerticalBar';
 
-export default function LanguageModule({navigation}) {
+export default function LanguageModule({ navigation }) {
   useEffect(() => {
     const lockOrientation = async () => {
-      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.LANDSCAPE
+      );
     };
 
     lockOrientation();
@@ -22,19 +32,66 @@ export default function LanguageModule({navigation}) {
   }, []);
 
   const wordsarrtemp = [
-    { wordname: 'I', wordtype: 'pronoun' , url: require('@/assets/images/I.jpeg') },
-    { wordname: 'want', wordtype: 'verb', url: require('@/assets/images/Want.jpeg')  },
-    { wordname: 'to', wordtype: 'preposition',url: require('@/assets/images/Want.jpeg')  },
-    { wordname: 'eat', wordtype: 'verb',url: require('@/assets/images/Eat.jpeg')  },
-    { wordname: 'Drink', wordtype: 'adjective',url: require('@/assets/images/Drink.jpeg')  },
-    { wordname: 'happy', wordtype: 'adjective',url: require('@/assets/images/Happy.jpeg')  },
-    { wordname: 'am', wordtype: 'prepositiion',url: require('@/assets/images/I.jpeg')  },
-    { wordname: 'tired', wordtype: 'adjective',url: require('@/assets/images/Tired.jpeg')  },
-    { wordname: 'learn', wordtype: 'verb',url: require('@/assets/images/Learn.jpeg')  },
-    { wordname: 'play', wordtype: 'verb',url: require('@/assets/images/Play.jpeg')  },
-    { wordname: 'run', wordtype: 'verb',url: require('@/assets/images/Want.jpeg')  },
-    { wordname: 'very', wordtype: 'adverb',url: require('@/assets/images/Want.jpeg')  },
-   
+    {
+      wordname: "I",
+      wordtype: "pronoun",
+      url: require("@/assets/images/I.jpeg"),
+    },
+    {
+      wordname: "want",
+      wordtype: "verb",
+      url: require("@/assets/images/Want.jpeg"),
+    },
+    {
+      wordname: "to",
+      wordtype: "preposition",
+      url: require("@/assets/images/Want.jpeg"),
+    },
+    {
+      wordname: "eat",
+      wordtype: "verb",
+      url: require("@/assets/images/Eat.jpeg"),
+    },
+    {
+      wordname: "Drink",
+      wordtype: "adjective",
+      url: require("@/assets/images/Drink.jpeg"),
+    },
+    {
+      wordname: "happy",
+      wordtype: "adjective",
+      url: require("@/assets/images/Happy.jpeg"),
+    },
+    {
+      wordname: "am",
+      wordtype: "prepositiion",
+      url: require("@/assets/images/I.jpeg"),
+    },
+    {
+      wordname: "tired",
+      wordtype: "adjective",
+      url: require("@/assets/images/Tired.jpeg"),
+    },
+    {
+      wordname: "learn",
+      wordtype: "verb",
+      url: require("@/assets/images/Learn.jpeg"),
+    },
+    {
+      wordname: "play",
+      wordtype: "verb",
+      url: require("@/assets/images/Play.jpeg"),
+    },
+    {
+      wordname: "run",
+      wordtype: "verb",
+      url: require("@/assets/images/Want.jpeg"),
+    },
+    {
+      wordname: "very",
+      wordtype: "adverb",
+      url: require("@/assets/images/Want.jpeg"),
+    },
   ];
 
   // Function to group words into rows of 5
@@ -47,65 +104,76 @@ export default function LanguageModule({navigation}) {
   };
 
   const rows = chunkArray(wordsarrtemp, 5); // Create rows of 5 card
-  const[WordToSpeak,SetWordToSpeak]=useState([])
-  const speakallwords=()=>
-  {
+  const [WordToSpeak, SetWordToSpeak] = useState([]);
+  const speakallwords = () => {
     //WordToSpeak.forEach((word)=>
     //{
-      //speakWord(word)
+    //speakWord(word)
     //})//
-    speakWord(WordToSpeak.toString())
-  }
-  
-  const handledelete=()=>
-  {
-    if(WordToSpeak.length>0)
-    {
-      SetWordToSpeak((prev)=>
-      {
-        return  prev.slice(0,-1)
-      })
+    speakWord(WordToSpeak.toString());
+  };
+
+  const handledelete = () => {
+    if (WordToSpeak.length > 0) {
+      SetWordToSpeak((prev) => {
+        return prev.slice(0, -1);
+      });
     }
-  }
+  };
 
-  const handleclear=()=>
-  {
-    SetWordToSpeak([])
-  }
+  const handleclear = () => {
+    SetWordToSpeak([]);
+  };
 
-  
   return (
-    
     <View style={styles.container}>
-      
       <TouchableOpacity
-          className="absolute top-0 left-0 mt-5 ml-5"
-          onPress={() => navigation.navigate('WordLevel')} // Replace 'NextPage' with your actual route name
-        >
-          <Image source={arrow_left} className="w-10 h-10" />
-        </TouchableOpacity>
+        className="absolute top-0 left-0 mt-5 ml-5"
+        onPress={() => navigation.navigate("WordLevel")} // Replace 'NextPage' with your actual route name
+      >
+        <Image source={arrow_left} className="w-10 h-10" />
+      </TouchableOpacity>
       {/* Bar container */}
-      <View style={styles.barContainer}>
-        
-        {/* Word to be spoken */}
-        <Text style={styles.wordText}>Word to speak:{WordToSpeak.join(' ')}</Text>
 
-        {/* Buttons container */}
-        <View style={styles.buttonContainer}>
-          {/* Speak button */}
-          <TouchableOpacity style={styles.button} onPress={speakallwords} >
-            <Text style={styles.buttonText} className="text-white">Speak</Text>
-          </TouchableOpacity>
+     
+        <View style={styles.barContainer}>
+          {/* Word to be spoken */}
+          <Text style={styles.wordText}>
+            Word to speak:{WordToSpeak.join(" ")}
+          </Text>
 
-          {/* Delete button */}
-          <TouchableOpacity style={styles.button} onPress={handledelete}>
-            <Text style={styles.buttonText} className="text-white">Delete</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={handleclear}>
-            <Text style={styles.buttonText} className="text-white">Clear</Text>
-          </TouchableOpacity>
-        </View>
+          {/* Buttons container */}
+          <View style={styles.buttonContainer}>
+            {/* Speak button */}
+            <TouchableOpacity style={styles.button} onPress={speakallwords}>
+              <Text style={styles.buttonText} className="text-white">
+                Speak
+              </Text>
+            </TouchableOpacity>
+
+            {/* Delete button */}
+            <TouchableOpacity style={styles.button} onPress={handledelete}>
+              <Text style={styles.buttonText} className="text-white">
+                Delete
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleclear}>
+              <Text style={styles.buttonText} className="text-white">
+                Clear
+              </Text>
+            </TouchableOpacity>
+          </View>
+ 
+       
       </View>
+      <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert("Sliding Menu")}
+          className="absolute top-0 right-3 mt-5 ml-5"
+        >
+          <Icon name="menu" size={24} color="#fff" style={styles.icon} />
+        
+        </TouchableOpacity>
 
       {/* Vertical Scrollable Language Cards */}
       <FlatList
@@ -113,7 +181,14 @@ export default function LanguageModule({navigation}) {
         renderItem={({ item }) => (
           <View style={styles.rowContainer}>
             {item.map((word, index) => (
-              <LanguageCards key={index} wordName={word.wordname} wordType={word.wordtype}  url={word.url} SetWordToSpeak={SetWordToSpeak} WordToSpeak={WordToSpeak}/>
+              <LanguageCards
+                key={index}
+                wordName={word.wordname}
+                wordType={word.wordtype}
+                url={word.url}
+                SetWordToSpeak={SetWordToSpeak}
+                WordToSpeak={WordToSpeak}
+              />
             ))}
           </View>
         )}
@@ -121,11 +196,7 @@ export default function LanguageModule({navigation}) {
         contentContainerStyle={styles.scrollContainer}
         showsVerticalScrollIndicator={false}
       />
-      
-   
     </View>
-    
-  
   );
 }
 
@@ -134,31 +205,31 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 2,
     paddingTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center', // Center the bar container
-    backgroundColor: '#f5f5f5',
+    justifyContent: "center",
+    alignItems: "center", // Center the bar container
+    backgroundColor: "#f5f5f5",
   },
   barContainer: {
-    width: '80%', // Increased width for centering
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "80%", // Increased width for centering
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 15,
-    backgroundColor: '#e0e0e0',
+    backgroundColor: "#e0e0e0",
     borderRadius: 10,
     elevation: 2,
     marginBottom: 10, // Add some margin below the bar
   },
   wordText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: "bold",
+    color: "#333",
   },
   buttonContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: "#007bff",
     paddingVertical: 8,
     paddingHorizontal: 16,
     borderRadius: 5,
@@ -166,12 +237,11 @@ const styles = StyleSheet.create({
   },
   scrollContainer: {
     padding: 10,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    marginBottom: 10, 
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginBottom: 10,
   },
-
 });
---
+++ manjari_frontend/components/Sliding_Menu/SlidingMenu.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SlidingMenu() {
+  return (
+    <View >
+      <Text>SlidingMenu</Text>
+    </View>
+  )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        width: "60%",
+        height: "100%",
+        backgroundColor: "#FFB6C1"
+    }
+})
